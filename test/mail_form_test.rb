b254c12a2d104e89a2e5675d14612a3b21e93295
Ram Iyer <ramky.iyer@yahoo.com>
2015-01-28 10:28:16 -0800

    adding first test that allows one to specify which attributes the mail form object contains

--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
+require 'fixtures/sample_mail'
 
 class MailFormTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, MailForm
+  end
+
+  test 'sample mail has name and email as attributes' do
+    sample = SampleMail.new
+    sample.name = 'User'
+    sample.email = 'user@example.com'
+
+    assert_equal 'User', sample.name
+    assert_equal 'user@example.com', sample.email
   end
 end
